e35847ba3e824893c39a1ded34b406fb8fb59ab3
Yifei <yifeizhou.to@gmail.com>
2019-07-19 17:40:14 -0400

    Set up servers

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+    console.log("blahblah")
+
+  }
+
+  callAPI() {
+      fetch("http://localhost:3001/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentDidMount() {
+      console.log("blahblah")
+      this.callAPI();
+      this.socket = new WebSocket('ws://localhost:3001/');
+        this.socket.addEventListener('open', () => {
+            console.log('Connected to server');
+        });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">{this.state.apiResponse}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
