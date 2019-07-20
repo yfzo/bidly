bcebcedfbd87c906b55d274d6aab3d17a886e708
asucarlos <kuwahara.mee@gmail.com>
2019-07-20 14:03:36 -0400

    component setup example

--- client/src/App.js
+++ client/src/App.js
@@ -27,13 +27,19 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">{this.state.apiResponse}</p>
-      </div>
+      // <div className="App">
+      //   <div className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h2>Welcome to React</h2>
+      //   </div>
+      //   <p className="App-intro">{this.state.apiResponse}</p>
+      // </div>
+      
+      <Router>
+        <Route path="/" component={Home} />
+        <Route path="/login" component={Login} />
+
+      </Router>
     );
   }
 }
---
+++ client/src/containers/Login.jsx
@@ -0,0 +1,25 @@
+export default class Login {
+  login = (e) => {
+    axios.post('/api/login')
+    fetch()
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Login!</h1>
+        <LoginForm onSubmit={this.login} />
+      </div>
+    )
+  }
+}
+
+
+// Dumb Component
+export default class LoginForm {
+  render() {
+    return (
+      <form onSubmit={this.props.onSubmit}></form>
+    )
+  }
+}
