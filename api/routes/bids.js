6550f5a0b969281e79c030a3e6395cadeddac57b
asucarlos <kuwahara.mee@gmail.com>
2019-07-23 13:43:52 -0400

    complete login form logic

--- api/routes/bids.js
+++ api/routes/bids.js
@@ -1,23 +1,23 @@
 var express = require("express");
 var router = express.Router();
+const uuid = require("uuid");
 
 module.exports = (knex) => {
 
 router.post("/", function(req, res, next) {
-    console.log('reached post route')
-    console.log(req.body)
-    if (JSON.stringify(req.body) !== '{}' && req.body.auction_id && req.body.user_id && req.body.amount) {
-        knex('bids').insert({
-            id: 1, //for now
-            auction_id : req.body.auction_id,
-            user_id: req.body.user_id,
-            amount: req.body.amount
-        }).then(() => {
-            console.log('bid posted')
-        }).catch((err) => {
-            console.log(err)
-        })
-    }
+    console.log('reached post bids route')
+    // if (JSON.stringify(req.body) !== '{}' && req.body.auction_id && req.body.user_id && req.body.amount) {
+    //     knex('bids').insert({
+    //         id: 2, //for now
+    //         auction_id : req.body.auction_id,
+    //         user_id: req.body.user_id,
+    //         amount: req.body.amount
+    //     }).then(() => {
+    //         console.log('bid posted')
+    //     }).catch((err) => {
+    //         console.log(err)
+    //     })
+    // }
 });
 
 return router
--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -1,17 +1,54 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import LoginForm from '../components/LoginForm.jsx'; 
 import '../login.css';
+import { Redirect } from 'react-router-dom';
+
 
 export default class Login extends Component {
-  // login = (e) => {
-  //   axios.post('/api/login')
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false,
+    }
+  }
+
+  login = (email, password) => {
+    if (!email || !password) {
+      console.log('no info')
+    } else {
+      const user = {
+        email: email,
+        password: password,
+      }
+    console.log('reached login frontend' + email + password)
+    fetch("http://localhost:3000/login", {
+      method: 'POST',
+      body: JSON.stringify(user), 
+      headers: {"Content-Type": "application/json"}
+    })
+    .then(function(response){
+      if(response.ok){
+        return response.json()
+      } else {
+        throw Error(`Request rejected with status ${response.status}`);
+      }
+    }).then(function(body){
+      console.log(body)
+    }).catch((err) => console.log('error' + err))
+
+    this.setState({ redirect: true});
+    }
+  }
 
   render() {
+    if (this.state.redirect == true ) {
+      return <Redirect to='/' />
+    }
     return (
       <div>
         <h2>Sign in</h2>
-        <LoginForm onSubmit={this.login} />
+        <LoginForm onSubmit={(email, password) => {
+          this.login(email, password) }} filledform={this.state.filledform} />
       </div>
     )
   }
