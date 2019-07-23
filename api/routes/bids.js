f1cbe5ad12dd9e7a69bf7243fcb86828a36ea0ae
asucarlos <kuwahara.mee@gmail.com>
2019-07-23 13:47:16 -0400

    complete login route

--- api/routes/bids.js
+++ api/routes/bids.js
@@ -6,18 +6,18 @@ module.exports = (knex) => {
 
 router.post("/", function(req, res, next) {
     console.log('reached post bids route')
-    // if (JSON.stringify(req.body) !== '{}' && req.body.auction_id && req.body.user_id && req.body.amount) {
-    //     knex('bids').insert({
-    //         id: 2, //for now
-    //         auction_id : req.body.auction_id,
-    //         user_id: req.body.user_id,
-    //         amount: req.body.amount
-    //     }).then(() => {
-    //         console.log('bid posted')
-    //     }).catch((err) => {
-    //         console.log(err)
-    //     })
-    // }
+    if (JSON.stringify(req.body) !== '{}' && req.body.auction_id && req.body.user_id && req.body.amount) {
+        knex('bids').insert({
+            id: 2, //for now
+            auction_id : req.body.auction_id,
+            user_id: req.body.user_id,
+            amount: req.body.amount
+        }).then(() => {
+            console.log('bid posted')
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
 });
 
 return router
