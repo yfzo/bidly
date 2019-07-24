1d02a1c1e324ee5727d0791264af491a36bc7ea5
Yifei <yifeizhou.to@gmail.com>
2019-07-24 10:54:46 -0400

    Removed bid insertion with id defined

--- api/routes/bids.js
+++ api/routes/bids.js
@@ -8,7 +8,7 @@ router.post("/", function(req, res, next) {
     console.log('reached post bids route')
     if (JSON.stringify(req.body) !== '{}' && req.body.auction_id && req.body.user_id && req.body.amount) {
         knex('bids').insert({
-            id: 2, //for now
+            // id: 2, //for now
             auction_id : req.body.auction_id,
             user_id: req.body.user_id,
             amount: req.body.amount
