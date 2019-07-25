e7573b1eeacc3844a53a6789fa9d2f8b0ddfe026
asucarlos <kuwahara.mee@gmail.com>
2019-07-25 12:25:56 -0400

    fix route problem

--- client/src/containers/AuctionDetail.jsx
+++ client/src/containers/AuctionDetail.jsx
@@ -24,7 +24,9 @@ export default class AuctionDetail extends Component {
   }
 
   componentDidMount() {
-      this.callAPI();
+      if(this.props.match.params.id !== 'new'){
+        this.callAPI();
+      }
   }
   
   bidHandler = (num) => {
@@ -63,12 +65,15 @@ export default class AuctionDetail extends Component {
 
   render() {
 
-    console.log("This is auction", this.state.auction)
-    if (this.state.redirect) {
-      console.log(`**********/auctions/${this.state.auction.id + 1}`)
-      return <Redirect push to={`/auctions/${this.state.auction.id + 1}`} />
+    // console.log("This is auction", this.state.auction)
+    // if (this.state.redirect) {
+    //   console.log(`**********/auctions/${this.state.auction.id + 1}`)
+    //   return <Redirect push to={`/auctions/${this.state.auction.id + 1}`} />
+    // }
+
+    if(this.props.match.params.id === 'new'){
+      return (<React.Fragment></React.Fragment>)
     }
-        
     var currentTime = new Date();
     var minutes = 1;
     var futureTime = currentTime.getTime() + (minutes * 60000)
