3182ee90c3714efe7f56eec217bc06bbd6e98bae
asucarlos <kuwahara.mee@gmail.com>
2019-07-25 18:18:56 -0400

    navbar authentication

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,12 +1,5 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-// import Logout from './Logout.jsx'
-import Auctions from '../containers/Auctions.jsx';
-import Login from '../containers/Login.jsx';
-import Register from '../containers/Register.jsx';
-import Home from '../containers/Home.jsx';
-import NewAuction from '../containers/NewAuction.jsx';
-import { Redirect } from 'react-router-dom';
 
 export default class NavBar extends Component {
   Logout = () => {
@@ -14,18 +7,27 @@ export default class NavBar extends Component {
     localStorage.clear()
     window.location.href = '/';
   }
-  
+
   render() {
-    return (
-      <div className="NavBar">
-        <div>This is navbar</div>
+    return (<div className="NavBar">
+
+    {localStorage.getItem('user_id') ? (
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/auctions">Browse Auctions</Link>
+        <Link to="/auctions/new">Create auction</Link>
+        <Link to="/users/:id">Profile</Link>
+        <button onClick={this.Logout}>Logout</button>
+      </div>
+      ) : (
+        <div>
         <Link to="/">Home</Link>
         <Link to="/auctions">Browse Auctions</Link>
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
-        <Link to="/auctions/new">Create auction</Link>
-        <button onClick={this.Logout}>Logout</button>
-      </div>
-    )
+        </div>
+      )
+    }
+      </div>)
   }
 }
