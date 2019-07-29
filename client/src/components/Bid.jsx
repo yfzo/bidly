5c7b88dfe4d4a2cd94e34d138493b679f77edc6d
Yifei <yifeizhou.to@gmail.com>
2019-07-29 14:30:48 -0400

    Styled bid button and input

--- client/src/components/Bid.jsx
+++ client/src/components/Bid.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export default class Bid extends Component {
   constructor(props) {
@@ -19,15 +21,26 @@ export default class Bid extends Component {
   render () {
     return (
       <div>
-        <button type="button" onClick={this.onClick}>Bid</button>
+        <div>
+          <Button variant="light" size="lg" block onClick={this.onClick}>
+            Make Bid
+          </Button>
+        </div>
         {this.state.showForm ? 
-        <form onSubmit={this.BidSubmitHandler}>
-        <label>
-          Bid amount:
-        </label>
-        <input type="money" name="bid_amount" placeholder="Type amount and hit ENTER"/>
-        </form>
+        // <form onSubmit={this.BidSubmitHandler}>
+        // <label className="bid-label">
+        //   How much would you like to bid?
+        // </label>
+        // <input type="money" name="bid_amount" placeholder="Type amount and hit ENTER"/>
+        // </form>
+        <Form onSubmit={this.BidSubmitHandler}>
+          <Form.Group controlId="bidInput">
+            <Form.Label className="bid-label">How much would you like to bid?</Form.Label>
+            <Form.Control className="bid-input" type="money" name="bid_amount" placeholder="Hit enter to submit" />
+          </Form.Group>
+        </Form>
         : null }
+        
       </div>
     ) 
   }
