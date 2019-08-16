6dab9e31b2fd3780be702994b17aa41f7d10e87b
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2019-08-16 19:00:31 -0400

    now redirects back to your profile page if you attempt to view someone else's profile page

--- client/src/containers/Profile.jsx
+++ client/src/containers/Profile.jsx
@@ -12,22 +12,31 @@ export default class Profile extends Component {
     console.log("blahblah")
   }
 
+  verifyToken(token) {
+    let theThing = null;
+    jwt.verify(token, 'super-secret-sauce', function(err, decoded){
+      theThing = decoded;
+    })
+    return theThing.userId;
+  }
+
   callAPI() {
+    const decodedId = this.props.match.params.id;
     const path = "http://localhost:3001/users/" + this.props.match.params.id;
     fetch(path)
           .then(res => res.json())
           .then(res => {
-            this.setState({ data: res })
+            if (this.verifyToken(localStorage.getItem('user_id')) === decodedId) {
+              this.setState({ data: res })
+            } else {
+              this.setState({data: this.verifyToken(localStorage.getItem('user_id'))})
+            }
           })
           .catch(err => (console.log('This is error' + err )))
           .then(res => ("this is data" + console.log(this.state.data)))
   }
 
-  verifyToken(token) {
-    jwt.verify(token, process.env.SECRET, function(err, decoded){
-      return decoded;
-    })
-  }
+
 
   componentDidMount() {
       console.log("blahblah")
@@ -37,6 +46,9 @@ export default class Profile extends Component {
     render() {
       if (this.state.redirect == true ) {
         return <Redirect to={'/auctions/' + this.verifyToken(localStorage.getItem('user_id'))}/>
+      } else if (typeof this.state.data === "string") {
+        this.callAPI()
+        return <Redirect to={'/users/' + this.verifyToken(localStorage.getItem('user_id'))}/>
       }
       console.log(this.state.data);
       return (
